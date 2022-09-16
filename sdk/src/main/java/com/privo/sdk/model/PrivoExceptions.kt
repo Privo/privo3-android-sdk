7f889badf1f07b055c6b3452a42aa71c855511ca
AlexSlo <aslobodeniuk@swiftinvention.com>
2022-09-16 11:34:11 +0300

    Improve error code

--- sdk/src/main/java/com/privo/sdk/model/PrivoExceptions.kt
+++ sdk/src/main/java/com/privo/sdk/model/PrivoExceptions.kt
@@ -1,4 +1,4 @@
 package com.privo.sdk.model
 
-public class NoInternetConnectionException() : Exception("No Internet connection")
+public class NoInternetConnectionException() : Exception("No Internet Connection")
 public class IncorrectDateOfBirthException() : Exception("Incorrect Date of Birth")
